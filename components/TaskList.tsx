d1d36565c6178e9d5500bdbdbaf5a7a5ef420d23
João Alves <joao.alves@ecosis.com.br>
2022-09-12 12:11:51 -0300

    Feat: create TaskList

---
+++ components/TaskList.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from "react-native"
+
+interface TaskListProps {}
+
+function TaskList(props: TaskListProps) {
+    return (
+        <View>
+            <Text>Task list</Text>
+        </View>
+    )
+}
+
+export default TaskList
